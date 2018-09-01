f89cbbc5278c3cf4ffbac5e1217cfc5201e96591
Peter Voivontas <petervoivontas@outlook.com>
2018-09-01 13:53:57 +0300

    New express request for updating user info

--- express.js
+++ express.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const signup = require('./users_mongodb_insert');
 const login = require('./users_mongodb_get');
+const update = require('./users_mongodb_update');
 const express = require('express');
 const app = express();
 
@@ -17,15 +18,22 @@ app.use(function(req, res, next) {
 
 app.post('/signup', (req, res, next) => {
     if (req.body.name && req.body.email && req.body.password) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-            const newUser = {
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
+        login.getUser(req.body.email);
+        setTimeout(() => {
+            if (login.result) {
+                res.send('Account already exists. Log in instead.');
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    const newUser = {
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: hash
+                    }
+                    signup.insertUsers(newUser);
+                    res.send(newUser);
+                });
             }
-            signup.insertUsers(newUser);
-            res.send(newUser);
-        });
+        }, 2000);
     } else if (req.body.name === '') {
         res.status(404).send('Name cannot be empty');
     } else if (req.body.email === '') {
@@ -59,6 +67,69 @@ app.post('/login', (req, res, next) => {
         res.send('Password cannot be blank');
     }
 });
+
+app.post('/updateName', (req, res, next) => {
+    if (req.body.oldEmail && req.body.newName) {
+        const query = {
+            email: req.body.oldEmail
+        }
+        const newName = {
+            $set: {
+                name: req.body.newName
+            }
+        }
+        update.updateName(query, newName);
+        setTimeout(() => {
+            res.send(update.result);
+        }, 2000);
+    }
+});
+
+app.post('/updateEmail', (req, res, next) => {
+    if (req.body.oldEmail && req.body.newEmail) {
+        const query = {
+            email: req.body.oldEmail
+        }
+        const newEmail = {
+            $set: {
+                email: req.body.newEmail
+            }
+        }
+        update.updateEmail(query, newEmail);
+        setTimeout(() => {
+            res.send(update.result);
+        }, 2000);
+    }
+});
+
+app.post('/updatePassword', (req, res, next) => {
+    if (req.body.oldEmail && req.body.oldPassword && req.body.newPassword) {
+        const query = {
+            email: req.body.oldEmail,
+        }
+        bcrypt.compare(req.body.newPassword, req.body.oldPassword, (err, result) => {
+            if (result) {
+                res.send('Password is the same as before');
+            } else {
+                bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        const newPassword = {
+                            $set: {
+                                password: hash
+                            }
+                        }
+                        update.updatePassword(query, newPassword);
+                    }
+                });
+            }
+        });
+        setTimeout(() => {
+            res.send(update.result);
+        }, 2000);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
