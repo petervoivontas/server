367b53e6b4e0a36a073d62db188749c2dcf33607
Peter Voivontas <petervoivontas@outlook.com>
2018-09-22 21:24:01 +0300

    sdaf

--- gcp_test.js
+++ gcp_test.js
@@ -3,6 +3,13 @@ const app = express();
 
 const PORT = 4000;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Cache-control", "no-cache, no-store");
+    next();
+});
+
 app.get('/test', (req, res, next) => {
     res.send('Hello world');
 });
