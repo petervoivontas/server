be2dbff1891a06e72c3f49222c8a6c43c95e3fd4
Peter Voivontas <petervoivontas@outlook.com>
2018-09-24 21:12:38 +0300

    sdf

--- gcp_test.js
+++ gcp_test.js
@@ -6,6 +6,7 @@ const PORT = 4000;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Upgrade-Insecure-Requests", "1")
     // res.header("Cache-control", "no-cache, no-store");
     next();
 });
